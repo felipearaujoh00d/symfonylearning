b42e733698f03fefc04b641672492e2936f5ee79
Felipe Araujo <felipearaujo.asti@gmail.com>
2018-03-19 10:48:51 -0300

    Generating Urls

--- symfony_lessons/development_symfony4/src/Aquanote/GunesBundle/Controller/GenusController.php
+++ symfony_lessons/development_symfony4/src/Aquanote/GunesBundle/Controller/GenusController.php
@@ -31,4 +31,24 @@ class GenusController extends Controller
             ]
         );
     }
+
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($genusName)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' => $notes,
+        ];
+
+        return new JsonResponse($data);
+    }
 }
