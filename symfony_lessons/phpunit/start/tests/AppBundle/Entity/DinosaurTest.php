118f1e0dc7abb7a851c9bbbf31ab4a618a03e401
Felipe Araujo <felipearaujo.asti@gmail.com>
2018-05-23 22:07:47 -0300

    TDD

--- symfony_lessons/phpunit/start/tests/AppBundle/Entity/DinosaurTest.php
+++ symfony_lessons/phpunit/start/tests/AppBundle/Entity/DinosaurTest.php
@@ -34,7 +34,27 @@ class DinosaurTest extends TestCase
 
         $dinosaur->setLength(15);
 
-        $this->assertGreaterThan( 16, $dinosaur->getLength(), 'Did you put it in the washing machine?');
+        $this->assertGreaterThan( 12, $dinosaur->getLength(), 'Did you put it in the washing machine?');
+    }
+
+    public function testReturnsFullSpecificationOfDinosaur()
+    {
+        $dinosaur = new Dinosaur();
+        $this->assertSame(
+            'The Unknown non-carnivorous dinosaur is 0 meters long',
+            $dinosaur->getSpecification()
+        );
+    }
+
+    public function testReturnsFullSpecificationForTyrannosaurus()
+    {
+        $dinosaur = new Dinosaur('Tyrannosaurus', true);
+        $dinosaur->setLength(12);
+
+        $this->assertSame(
+            'The Tyrannosaurus carnivorous dinosaur is 12 meters long',
+            $dinosaur->getSpecification()
+        );
     }
 
 
